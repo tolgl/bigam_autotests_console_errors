6a2888dffaa5a1cdddbcfea52fc8f02bf9f730d5
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-15 18:04:01 +0300

    методы для шапки сайта

---
+++ pages/header.py
@@ -0,0 +1,14 @@
+import allure
+
+from locators.header_locators import HeaderLocators
+from pages.base_page import BasePage
+
+
+class HeaderHelper(BasePage):
+
+    def click_button_auth(self):
+        return self.find_element(locator=HeaderLocators.auth_link, wait_time=3).click()
+
+    @allure.step('Переходим в ЛК')
+    def click_link_personal_account(self):
+        return self.find_element(locator=HeaderLocators.link_personal_account, wait_time=3).click()
