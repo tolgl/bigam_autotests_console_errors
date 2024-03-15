8086d7fc1f61f3eb27516968f9a818be08fd1d91
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-15 18:04:41 +0300

    локаторы ЛК

---
+++ locators/personal_page_locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class PersonalPageLocators:
+    link_my_order = (By.XPATH, ".//a[@href='/personal/orders/']")
+    link_favorites = (By.XPATH, ".//a[@href='/personal/favorites/']")
+    link_history_product = (By.XPATH, ".//a[@href='/personal/history/']")
+    link_repairs = (By.XPATH, ".//a[@href='/personal/repairs/']")
+    link_reviews = (By.XPATH, ".//a[@href='/personal/reviews/']")
+    link_settings = (By.XPATH, ".//a[@href='/personal/settings/']")
