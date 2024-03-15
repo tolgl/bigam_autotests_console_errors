bb0d3399387f34e901e9d244accec8558e7dcf0d
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-15 17:44:20 +0300

    базовые локаторы

---
+++ locators/base_page_locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    modal_geo = (By.CLASS_NAME, "a-confirm-city-modal-content")
+    miss_click = (By.CLASS_NAME, "vm--overlay")
