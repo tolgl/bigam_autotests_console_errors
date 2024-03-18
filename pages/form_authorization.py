09833f57211afb9ef370ab3669d0dc5171345ab0
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-18 11:23:56 +0300

    добавлен метод авторизации б2б пользователя

--- pages/form_authorization.py
+++ pages/form_authorization.py
@@ -20,3 +20,13 @@ class FormAuthorizationHelper(BasePage):
     @allure.step('Нажимаем на кнопку "Войти"')
     def click_button_login(self):
         self.find_element(FormAuthLocators.button_login, wait_time=3).click()
+
+    @allure.step('Нажимаем на кнопку "Вход" для юр лиц')
+    def click_button_login_b2b(self):
+        self.find_element(FormAuthLocators.button_login_b2b, wait_time=3).click()
+
+    @allure.step('Заполняем форму авторизации б2б пользователя')
+    def filing_form_auth_b2b_user(self, email, password):
+        self.find_element(FormAuthLocators.field_email, wait_time=3).send_keys(email)
+        self.find_element(FormAuthLocators.field_password_b2b, wait_time=3).send_keys(password)
+        self.find_element(FormAuthLocators.button_login, wait_time=3).click()
