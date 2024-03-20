c697807593d72d356fc693141f469940cb3afd1c
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-20 17:16:54 +0300

    добавлен локатор лоадера

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     modal_geo = (By.CLASS_NAME, "a-confirm-city-modal-content")
     miss_click = (By.CLASS_NAME, "vm--overlay")
+    loader = (By.CLASS_NAME, "a-local-loader")
