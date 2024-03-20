103fc14384e07cbf05a8597627b626facc56ff53
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-20 17:17:22 +0300

    добавлен метод ожидания скрытия элемента

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,7 +9,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = 'https://bigam:12345@www.bigam-test.ru'
+        self.base_url = 'https://bigam:12345@php8.bigam-dev.ru'
 
     @allure.step('Открываем страницу')
     def go_to_page(self, path):
@@ -22,3 +22,7 @@ class BasePage:
     def miss_click_on_selection_city(self):
         self.find_element(BasePageLocators.modal_geo, wait_time=5)
         return self.find_element(BasePageLocators.miss_click, wait_time=5).click()
+
+    def wait_invisibility_element(self, locator, wait_time=15):
+        return WebDriverWait(self.driver, wait_time).until(
+            expected_conditions.invisibility_of_element(locator))
