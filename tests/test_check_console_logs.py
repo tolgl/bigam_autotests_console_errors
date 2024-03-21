5bd94d4674830a88cfa1c8dabbe009d254a39ac5
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-03-21 17:44:50 +0300

    добавлена очистка лога

--- tests/test_check_console_logs.py
+++ tests/test_check_console_logs.py
@@ -1,6 +1,6 @@
 import time
 
-from locators.base_page_locators import BasePageLocators
+from locators.form_authorization_locators import FormAuthLocators
 from pages.base_page import BasePage
 from pages.form_authorization import FormAuthorizationHelper
 from pages.header import HeaderHelper
@@ -9,7 +9,7 @@ from pages.personal_page import PersonalPageHelper
 
 class TestCheckConsoleLogs:
 
-    def test_check_console_errors_from_file_by_physical_person(self, driver, test_input):
+    def test_check_console_errors_from_file_by_physical_person(self, driver):
         base_page = BasePage(driver)
         base_page.go_to_page(path='')
         base_page.miss_click_on_selection_city()
@@ -20,12 +20,13 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
-        base_page.wait_invisibility_element(locator=BasePageLocators.loader)
-        driver.execute_script('console.clear()')
-        base_page.go_to_page(path=test_input)
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
+        base_page.go_to_page(path='/catalog/instrument-5748/?sort=price-desc')
         time.sleep(3)
         log = driver.get_log('browser')
-        print(log)
+
         assert (log == []) or (log[0]['level'] != 'SEVERE')
 
     def test_check_console_errors_from_file_by_b2b_user(self, driver, test_input):
@@ -38,8 +39,9 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
-        base_page.wait_invisibility_element(locator=BasePageLocators.loader)
-        driver.execute_script('console.clear()')
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         base_page.go_to_page(path=test_input)
         time.sleep(3)
         log = driver.get_log('browser')
@@ -57,6 +59,9 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         header.click_link_personal_account()
         time.sleep(3)
         log = driver.get_log('browser')
@@ -74,7 +79,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_my_order()
         time.sleep(3)
@@ -93,7 +101,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_favorites()
         time.sleep(3)
@@ -112,7 +123,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_history_product()
         time.sleep(3)
@@ -131,7 +145,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_repairs()
         time.sleep(3)
@@ -150,7 +167,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_reviews()
         time.sleep(3)
@@ -169,7 +189,10 @@ class TestCheckConsoleLogs:
         form_auth.filing_field_phone(phone='9201494057')
         form_auth.filing_field_password(password='123456')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_physical_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_settings()
         time.sleep(3)
@@ -187,6 +210,9 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         header.click_link_personal_account()
         time.sleep(3)
         log = driver.get_log('browser')
@@ -203,7 +229,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_my_organizations()
         time.sleep(3)
@@ -221,7 +250,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_my_order()
         time.sleep(3)
@@ -239,7 +271,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_favorites()
         time.sleep(3)
@@ -257,7 +292,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_history_product()
         time.sleep(3)
@@ -275,7 +313,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_repairs()
         time.sleep(3)
@@ -293,7 +334,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_reviews()
         time.sleep(3)
@@ -311,7 +355,10 @@ class TestCheckConsoleLogs:
         form_auth.click_button_login_b2b()
         form_auth.filing_form_auth_b2b_user(email='test_d@test.ru', password='87654321')
         form_auth.click_button_login()
+        base_page.wait_invisibility_element(locator=FormAuthLocators.h3_form_auth_b2b_user)
         header.click_link_personal_account()
+        # возвращает лог и очищает консоль
+        driver.get_log('browser')
         personal_page = PersonalPageHelper(driver)
         personal_page.click_link_settings()
         time.sleep(3)
